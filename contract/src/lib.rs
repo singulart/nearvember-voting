f4bc35c006e70e237915f031ef4d0926e51b3ee3
singulart <serpian@gmail.com>
2021-11-24 22:08:16 +0200

    voting app contract

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,204 +1,222 @@
-/*!
-Fungible Token implementation with JSON serialization.
-NOTES:
-  - The maximum balance value is limited by U128 (2**128 - 1).
-  - JSON calls should pass U128 as a base-10 string. E.g. "100".
-  - The contract optimizes the inner trie structure by hashing account IDs. It will prevent some
-    abuse of deep tries. Shouldn't be an issue, once NEAR clients implement full hashing of keys.
-  - The contract tracks the change in storage before and after the call. If the storage increases,
-    the contract requires the caller of the contract to attach enough deposit to the function call
-    to cover the storage cost.
-    This is done to prevent a denial of service attack on the contract by taking all available storage.
-    If the storage decreases, the contract will issue a refund for the cost of the released storage.
-    The unused tokens from the attached deposit are also refunded, so it's safe to
-    attach more deposit than required.
-  - To prevent the deployed contract from being modified or deleted, it should not have any access
-    keys on its account.
-*/
-use near_contract_standards::fungible_token::metadata::{
-    FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC,
-};
-use near_contract_standards::fungible_token::FungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LazyOption;
-use near_sdk::json_types::{ValidAccountId, U128};
-use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, Promise, PromiseOrValue};
+use near_sdk::{env, near_bindgen};
+use near_sdk::serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-near_sdk::setup_alloc!();
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-pub struct Contract {
-    token: FungibleToken,
-    metadata: LazyOption<FungibleTokenMetadata>,
+#[derive(Serialize, Deserialize, Clone, BorshDeserialize, BorshSerialize)]
+pub struct VotingOption {
+    option_id: String,
+    message: String,
 }
 
-const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+#[derive(Serialize, Deserialize, Clone, BorshDeserialize, BorshSerialize)]
+pub struct VotingOptions {
+    // Author of the vote (account id).
+    creator: String,
+    // Unique voting id.
+    poll_id: String,
+    // Question voted on.
+    question: String,
+    variants: Vec<VotingOption>,
+}
+
+#[derive(Serialize, Deserialize, Clone, BorshDeserialize, BorshSerialize)]
+pub struct VotingResults {
+    // Unique poll id.
+    poll_id: String,
+    // Map of option id to the number of votes.
+    variants: HashMap<String, i32>,
+    // Map of voters who already voted.
+    voted: HashMap<String, i32>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct VotingStats {
+    poll: VotingOptions,
+    results: VotingResults,
+}
 
 #[near_bindgen]
-impl Contract {
-    /// Initializes the contract with the given total supply owned by the given `owner_id` with
-    /// default metadata (for example purposes only).
-    #[init]
-    pub fn new_default_meta(owner_id: ValidAccountId, total_supply: U128) -> Self {
-        Self::new(
-            owner_id,
-            total_supply,
-            FungibleTokenMetadata {
-                spec: FT_METADATA_SPEC.to_string(),
-                name: "Example NEAR fungible token".to_string(),
-                symbol: "EXAMPLE".to_string(),
-                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
-                reference: None,
-                reference_hash: None,
-                decimals: 24,
-            },
-        )
-    }
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Voting {
+    // Map of poll id to voting options.
+    polls: HashMap<String, VotingOptions>,
+    // Map of poll id to voting results.
+    results: HashMap<String, VotingResults>,
+}
 
-    /// Initializes the contract with the given total supply owned by the given `owner_id` with
-    /// the given fungible token metadata.
-    #[init]
-    pub fn new(
-        owner_id: ValidAccountId,
-        total_supply: U128,
-        metadata: FungibleTokenMetadata,
-    ) -> Self {
-        assert!(!env::state_exists(), "Already initialized");
-        metadata.assert_valid();
-        let mut this = Self {
-            token: FungibleToken::new(b"a".to_vec()),
-            metadata: LazyOption::new(b"m".to_vec(), Some(&metadata)),
-        };
-        this.token.internal_register_account(owner_id.as_ref());
-        this.token.internal_deposit(owner_id.as_ref(), total_supply.into());
-        this
-    }
-
-    #[payable]
-    pub fn ft_mint(
-        &mut self,
-        receiver_id: AccountId,
-        amount: U128,
-    ) {
-        //get initial storage usage
-        assert!(
-            amount.0 <= 1000, 
-            "Cannot mint more than 1000 tokens"
-        ); 
-
-        let initial_storage_usage = env::storage_usage();
-
-        let mut amount_for_account = self.token.accounts.get(&receiver_id).unwrap_or(0); 
-        amount_for_account += amount.0; 
-
-        self.token.accounts.insert(&receiver_id, &amount_for_account);
-        self.token.total_supply = self
-            .token    
-            .total_supply
-            .checked_add(amount.0)
-            .unwrap_or_else(|| env::panic(b"Total supply overflow"));
-
-        //refund any excess storage
-        let storage_used = env::storage_usage() - initial_storage_usage;
-        let required_cost = env::storage_byte_cost() * Balance::from(storage_used);
-        let attached_deposit = env::attached_deposit();
-
-        assert!(
-            required_cost <= attached_deposit,
-            "Must attach {} yoctoNEAR to cover storage", required_cost
+#[near_bindgen]
+impl Voting {
+    pub fn vote(&mut self, poll_id: String, votes: HashMap<String, i32>) -> bool {
+        let voter_contract = env::signer_account_id();
+        let owner_contract = env::current_account_id();
+        env::log(
+            format!(
+                "{} is voting on {} owner is {}",
+                voter_contract, poll_id, owner_contract
+            )
+            .as_bytes(),
         );
+        // Now we need to find a contract to vote for.
+        match self.results.get_mut(&poll_id) {
+            Some(results) => {
+                match results.voted.get(&voter_contract) {
+                    Some(_) => {
+                        env::log(
+                            format!("{} already voted in {}", voter_contract, poll_id).as_bytes(),
+                        );
+                        return false;
+                    }
+                    None => {
+                        results.voted.insert(voter_contract, 1);
+                    }
+                }
+                for (vote, checked) in votes.iter() {
+                    if *checked == 0 {
+                        continue;
+                    }
+                    match results.variants.get_mut(vote) {
+                        Some(result) => {
+                            *result = *result + 1;
+                        }
+                        None => {
+                            results.variants.insert(vote.to_string(), 1);
+                        }
+                    }
+                }
+                return true;
+            }
+            None => {
+                env::log(format!("no poll known for {}", poll_id).as_bytes());
+                return false;
+            }
+        };
+    }
 
-        let refund = attached_deposit - required_cost;
-        if refund > 1 {
-            Promise::new(env::predecessor_account_id()).transfer(refund);
+    pub fn create_poll(&mut self, question: String, variants: HashMap<String, String>) -> String {
+        env::log(
+            format!(
+                "create_poll for {} currently have {} polls",
+                question,
+                self.polls.len()
+            )
+            .as_bytes(),
+        );
+        let creator_account_id = env::signer_account_id();
+        let poll_id = bs58::encode(env::sha256(&env::random_seed())).into_string();
+        let result = poll_id.clone();
+        let mut variants_vec = <Vec<VotingOption>>::new();
+        for (k, v) in variants.iter() {
+            variants_vec.push(VotingOption {
+                option_id: k.to_string(),
+                message: v.to_string(),
+            })
+        }
+        self.polls.insert(
+            poll_id.clone(),
+            VotingOptions {
+                creator: creator_account_id,
+                poll_id: poll_id.clone(),
+                question: question,
+                variants: variants_vec,
+            },
+        );
+        self.results.insert(
+            poll_id.clone(),
+            VotingResults {
+                poll_id: poll_id,
+                variants: HashMap::new(),
+                voted: HashMap::new(),
+            },
+        );
+        return result;
+    }
+
+    pub fn show_poll(&self, poll_id: String) -> Option<VotingOptions> {
+        match self.polls.get(&poll_id) {
+            Some(options) => Some(options.clone()),
+            None => {
+                env::log(format!("Unknown voting {}", poll_id).as_bytes());
+                None
+            }
         }
     }
 
-    fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
-        log!("Closed @{} with {}", account_id, balance);
+    pub fn show_results(&self, poll_id: String) -> Option<VotingStats> {
+        match self.polls.get(&poll_id) {
+            Some(poll) => match self.results.get(&poll_id) {
+                Some(results) => Some(VotingStats {
+                    results: results.clone(),
+                    poll: poll.clone(),
+                }),
+                None => None,
+            },
+            None => None,
+        }
     }
 
-    fn on_tokens_burned(&mut self, account_id: AccountId, amount: Balance) {
-        log!("Account @{} burned {}", account_id, amount);
+    pub fn ping(&self) -> String {
+        "PONG".to_string()
     }
 }
 
-near_contract_standards::impl_fungible_token_core!(Contract, token, on_tokens_burned);
-near_contract_standards::impl_fungible_token_storage!(Contract, token, on_account_closed);
-
-#[near_bindgen]
-impl FungibleTokenMetadataProvider for Contract {
-    fn ft_metadata(&self) -> FungibleTokenMetadata {
-        self.metadata.get().unwrap()
-    }
-}
-
-#[cfg(all(test, not(target_arch = "wasm32")))]
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
 mod tests {
-    use near_sdk::test_utils::{accounts, VMContextBuilder};
-    use near_sdk::MockedBlockchain;
-    use near_sdk::{testing_env, Balance};
-
     use super::*;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
 
-    const TOTAL_SUPPLY: Balance = 1_000_000_000_000_000;
-
-    fn get_context(predecessor_account_id: ValidAccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder
-            .current_account_id(accounts(0))
-            .signer_account_id(predecessor_account_id.clone())
-            .predecessor_account_id(predecessor_account_id);
-        builder
+    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+        VMContext {
+            current_account_id: "alice_near".to_string(),
+            signer_account_id: "bob_near".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: "carol_near".to_string(),
+            input,
+            block_index: 0,
+            epoch_height: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 0,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view,
+            output_data_receivers: vec![],
+        }
     }
 
     #[test]
-    fn test_new() {
-        let mut context = get_context(accounts(1));
-        testing_env!(context.build());
-        let contract = Contract::new_default_meta(accounts(1).into(), TOTAL_SUPPLY.into());
-        testing_env!(context.is_view(true).build());
-        assert_eq!(contract.ft_total_supply().0, TOTAL_SUPPLY);
-        assert_eq!(contract.ft_balance_of(accounts(1)).0, TOTAL_SUPPLY);
+    fn nonexisting_poll() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let contract = Voting::default();
+        let options = contract.show_poll("default".to_string());
+        assert_eq!(true, options.is_none());
     }
 
     #[test]
-    #[should_panic(expected = "The contract is not initialized")]
-    fn test_default() {
-        let context = get_context(accounts(1));
-        testing_env!(context.build());
-        let _contract = Contract::default();
-    }
-
-    #[test]
-    fn test_transfer() {
-        let mut context = get_context(accounts(2));
-        testing_env!(context.build());
-        let mut contract = Contract::new_default_meta(accounts(2).into(), TOTAL_SUPPLY.into());
-        testing_env!(context
-            .storage_usage(env::storage_usage())
-            .attached_deposit(contract.storage_balance_bounds().min.into())
-            .predecessor_account_id(accounts(1))
-            .build());
-        // Paying for account registration, aka storage deposit
-        contract.storage_deposit(None, None);
-
-        testing_env!(context
-            .storage_usage(env::storage_usage())
-            .attached_deposit(1)
-            .predecessor_account_id(accounts(2))
-            .build());
-        let transfer_amount = TOTAL_SUPPLY / 3;
-        contract.ft_transfer(accounts(1), transfer_amount.into(), None);
-
-        testing_env!(context
-            .storage_usage(env::storage_usage())
-            .account_balance(env::account_balance())
-            .is_view(true)
-            .attached_deposit(0)
-            .build());
-        assert_eq!(contract.ft_balance_of(accounts(2)).0, (TOTAL_SUPPLY - transfer_amount));
-        assert_eq!(contract.ft_balance_of(accounts(1)).0, transfer_amount);
+    fn create_poll() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let mut contract = Voting::default();
+        let poll = contract.create_poll(
+            "To be or not to be?".to_string(),
+            [
+                ("v1".to_string(), "To be".to_string()),
+                ("v2".to_string(), "Not to be".to_string()),
+            ]
+            .iter()
+            .cloned()
+            .collect(),
+        );
+        let options = contract.show_poll(poll);
+        assert_eq!(false, options.is_none());
+        assert_eq!("To be or not to be?".to_string(), options.unwrap().question);
     }
 }
